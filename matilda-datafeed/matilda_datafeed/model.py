c086f82c8a0fe502fad2a088f91cbd23d4c3983a
Chintalapati <achintalapati@cnet-global.com>
2020-03-25 09:41:53 -0500

    Updates

--- matilda-datafeed/matilda_datafeed/model.py
+++ matilda-datafeed/matilda_datafeed/model.py
@@ -24,4 +24,52 @@ class Alert():
     serviceId = None
     type = None
 
+class Application():
 
+    applicationId = None
+    clientApplicationId = None
+    name = None
+    organization = None
+    portfolioOwner = None
+    owner = None
+    director = None
+    custodian = None
+    category = None
+    criticality = None
+
+class Host():
+
+    hostId = None
+    state = None
+    datacenter = None
+    hostname = None
+    externalSerialNumber = None
+    instanceType = None
+    platform = None
+    make = None
+    model = None
+    ipAddress = None
+    ipDomain = None
+    operatingSystem = None
+    osVersion = None
+    cloudProvider = None
+    environment = None
+    deviceType = None
+    cores = None
+    allocatedMemory = None
+    allocatedStorage = None
+    osEosl = None
+    hwEosl = None
+
+class ApplicationHost():
+
+    applicationId = None
+    environmentId = None
+    hostId = None
+
+class Environment():
+
+    environmentId = None
+    applicationId = None
+    name = None
+    type = None
